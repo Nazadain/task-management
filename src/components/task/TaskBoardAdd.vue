913b279d925786c40321bb96fd273bfb79a578af
Nazadain <oldfather3@mail.ru>
2025-05-15 23:11:16 +0500

    Сreating add board logic

--- src/components/task/TaskBoardAdd.vue
+++ src/components/task/TaskBoardAdd.vue
@@ -5,8 +5,36 @@
 <template>
 <div class="board-add__wrapper">
   <div class="board-add_container">
-    <form action="">
+    <form>
+      <div class="form-item">
+        <label for="title">Название</label>
+        <input
+            id="title"
+            type="text"
+            name="title"
+            placeholder="Введите название"
+        >
+      </div>
 
+      <div class="form-item">
+        <label for="description">Описание</label>
+        <input
+            id="description"
+            type="text"
+            name="description"
+            placeholder="Введите описание"
+        >
+      </div>
+
+      <div class="form-item">
+        <label for="isPrivate">Приватная</label>
+        <input
+            id="isPrivate"
+            type="checkbox"
+            name="isPrivate"
+            placeholder="Введите описание"
+        >
+      </div>
     </form>
   </div>
 </div>
@@ -24,7 +52,7 @@
   left: 0;
   right: 0;
   background: #11111120;
-  z-index: 1000;
+  z-index: 2000;
 }
 
 .board-add_container {
